507cbcd53127fe1a5d81c12445209b6123de140d
Geoffrey Gamble <geoffreygamble.ucsd@gmail.com>
2016-02-25 15:45:50 -0800

    Some initial changes regarding the new testing framework ,we should discuss if my approach is good. Also, had to untangle some conflicts, we should also discuss this.

--- client/modules/magic/actions/tests/parse_contribution.js
+++ client/modules/magic/actions/tests/parse_contribution.js
@@ -18,7 +18,7 @@ const parseContributionErrorTest = (text, reErrorMsg) => {
   const LocalState_val = args[1];
 
   // expect the parse errors to contain one error that matches the matchErrorMSG regex
-  expect(LocalState_key).to.be.equal('PARSE_CONTRIBUTION_ERRORS');
+  expect(LocalState_key).to.be.equal('PARSE_CONTRIBUTION_ERROR');
   expect(LocalState_val).to.match(reErrorMsg);
 
 };
@@ -26,13 +26,30 @@ const parseContributionErrorTest = (text, reErrorMsg) => {
 describe('magic.actions.parseContribution', () => {
   describe('parse', () => {
     it('should reject if text is not there', () => {
-      parseContributionErrorTest(null, /required/);
+      parseContributionErrorTest(null, /Contribution text is required./);
     });
-    it('should reject nonsense', () => {
-      parseContributionErrorTest('nonsense', /not recognized as a column delimiter/);
+
+    it('should reject nonsense with no tab header', () => {
+      parseContributionErrorTest('nonsense', /Start of table string \"tab\" expected but not found./);
     });
-    it('should reject if table name is missing', () => {
-      parseContributionErrorTest('tab\n', /table name could not be found/);
+
+    it('should reject nonsense with tab header', () => {
+      parseContributionErrorTest('nonsense\ttable', /Start of table string \"tab\" expected but not found./);
     });
+
+    it('should reject leading space nonsense with tab and column headers', () => {
+      parseContributionErrorTest('  nonsense  \ttable\ncol1\tcol2\nstr1\t1.2', /Start of table string \"tab\" expected but not found./);
+    });
+
+    var noTableNames = ["tab\n",
+        " tab \n",
+        " tab \t",
+        "tab\t\n"];
+    for (var noTableName of noTableNames)
+    {
+      it('should reject if table name is missing', () => {
+        parseContributionErrorTest(noTableName, /Error, table delimiter \"tab\" has no table name./);
+      });
+    }
   });
 });
