287790de60b29d5b569a5b93c8ab2d95fbf79985
Rupert Minnett <rminnett@gmail.com>
2017-06-13 22:01:52 -0700

    Improve private contribution DOI assignment

--- server/methods/magic.js
+++ server/methods/magic.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import Fiber from 'fibers';
 import uuid from 'uuid';
+import promise from 'bluebird';
 import request from 'request';
 import elasticsearch from 'elasticsearch';
 //import s3 from 's3';
@@ -82,17 +83,6 @@ export default function () {
 
       c._id = uuid.v4();
 
-      //if (c.measurements && c.measurements.rows && c.measurements.columns) {
-      //  let i = 0;
-      //  for (i = 0; i < c.measurements.rows.length; i+=1000) {
-      //    Collections['magic.private.measurements'].insert({
-      //      contribution_mongo_id: c._id,
-      //      columns: c.measurements.columns,
-      //      rows: c.measurements.rows.slice(i, 1000)
-      //    });
-      //  }
-      //}
-
       s = s || {};
       s.contribution = s.contribution || {};
       s.contribution.TITLE = c._name;
@@ -143,151 +133,69 @@ export default function () {
 
       Collections['magic.private.contributions'].update(id, c, (error) => { console.log('update', error, id, _.keys(c))});
     },
-    'activateContribution': function (id) {
+    async activateContribution(id) {
       let c = Collections['magic.private.contributions'].findOne(id);
       c._es_id = uuid.v4();
       c._activated = true;
-      Collections['magic.private.contributions'].update(id, c, (error) => { console.log('activate', error)});
+      await Collections['magic.private.contributions'].update(id, c, (error) => { console.log('activate', error)});
 
-      if (c && c.contribution && c.contribution[0].doi) {
-        esClient.search({
-          index: 'magic_v5', type: 'contributions_summaries',
-          body: {
-            "size": 1,
-            "query": {
-              "bool": {
-                "must": {
-                  "simple_query_string": {
-                    "query": '"' + c.contribution[0].doi + '"'
-                  }
-                },
-                "filter": {
-                  "term": {"UPLOAD": 1}
-                }
-              }
-            }
-          }
-        }).then((resp) => {
+      let summary = _.pick(c._summary.contribution, [
+        "AGE_UNIT",
+        "LAT_N",
+        "N_LOCATION_RESULTS",
+        "LITHOLOGIES",
+        "MEAS_STEP_MAX",
+        "AGES",
+        "GEOLOGIC_TYPES",
+        "N_SITES",
+        "N_SPECIMENS",
+        "LON_W",
+        "CONTRIBUTOR_ID",
+        "CITATIONS",
+        "LAT_S",
+        "CONTRIBUTOR",
+        "N_SAMPLES",
+        "N_SAMPLE_RESULTS",
+        "LON_E",
+        "N_LOCATIONS",
+        "METHOD_CODES",
+        "MEAS_STEP_UNIT",
+        "LOCATION_TYPE",
+        "GEOLOGIC_CLASSES",
+        "N_MEASUREMENTS",
+        "N_SPECIMEN_RESULTS",
+        "N_EXPERIMENTS",
+        "TITLE",
+        "MEAS_STEP_MIN",
+        "N_SITE_RESULTS",
+        "CITATION",
+        "YEAR",
+        "REFERENCE_KEYWORDS",
+        "MAGIC_CONTRIBUTION_ID",
+        "JOURNAL",
+        "REFERENCE_HTML",
+        "VERSION",
+        "version_history"
+      ]);
+      summary.UPLOAD = 1;
+      summary.INSERTED = moment().utc().format("DD-MMM-YY HH:mm:ss");
+      summary.MONGO_ID = id;
+      summary.version_history.unshift({
+        "contributor": summary.CONTRIBUTOR,
+        "upload": 1,
+        "contribution_id": summary.MAGIC_CONTRIBUTION_ID,
+        "version": summary.VERSION,
+        "magic_version": 3,
+        "activated": moment().utc()
+      });
 
-          c._summary.contribution.VERSION = '1';
-          resp.hits.hits.forEach((hit) => {
-            c._summary.prev_version = hit._source.VERSION;
-            c._summary.prev_contributor_id = hit._source.CONTRIBUTOR_ID;
-            c._summary.prev_contributor = hit._source.CONTRIBUTOR;
-            if (c._summary.prev_version)
-              c._summary.contribution.VERSION = Number(c._summary.prev_version) + 1 + '';
-          });
-
-          let summary = _.pick(c._summary.contribution, [
-            "AGE_UNIT",
-            "LAT_N",
-            "N_LOCATION_RESULTS",
-            "LITHOLOGIES",
-            "MEAS_STEP_MAX",
-            "AGES",
-            "GEOLOGIC_TYPES",
-            "N_SITES",
-            "N_SPECIMENS",
-            "LON_W",
-            "CONTRIBUTOR_ID",
-            "CITATIONS",
-            "LAT_S",
-            "CONTRIBUTOR",
-            "N_SAMPLES",
-            "N_SAMPLE_RESULTS",
-            "LON_E",
-            "N_LOCATIONS",
-            "METHOD_CODES",
-            "MEAS_STEP_UNIT",
-            "LOCATION_TYPE",
-            "GEOLOGIC_CLASSES",
-            "N_MEASUREMENTS",
-            "N_SPECIMEN_RESULTS",
-            "N_EXPERIMENTS",
-            "TITLE",
-            "MEAS_STEP_MIN",
-            "N_SITE_RESULTS",
-            "CITATION",
-            "YEAR",
-            "REFERENCE_KEYWORDS",
-            "MAGIC_CONTRIBUTION_ID",
-            "JOURNAL",
-            "REFERENCE_HTML",
-            "FILE_NAME"
-          ]);
-          summary.UPLOAD = 1;
-          summary.INSERTED = moment().format("DD-MMM-YY HH:mm:ss");
-          //summary.MONGO_ID = id;
-
-          //console.log('insertContribution', c.contribution);
-          esClient.index({
-            index: 'magic_v5', type: 'contributions_summaries',
-            id: id,
-            body: summary,
-          }, function (error, response) {
-            if (error) {
-              console.trace(error.message);
-              //this.error(new Meteor.Error(e, 'hey!'));
-            }
-          });
-
-        }, function (error) {
-          if (error) {
-            console.trace(error.message);
-            //this.error(new Meteor.Error(e, 'hey!'));
-          }
-        });
-      } else {
-        if (error) {
-          console.trace(error.message);
-          //this.error(new Meteor.Error(e, 'hey!'));
-        }
-      }
+      await esClient.index({
+        index: 'magic_v5', type: 'contributions_summaries',
+        id: id,
+        body: summary,
+      });
     },
-    'updatePrevious': function(id) {
-      let c = Collections['magic.private.contributions'].findOne(id);
-      if (c && c.contribution && c.contribution[0].doi) {
-        esClient.search({
-          index: 'magic_v5', type: 'contributions_summaries',
-          body: {
-            "size": 1,
-            "query": {
-              "bool": {
-                "must": {
-                  "simple_query_string": {
-                    "query": '"' + c.contribution[0].doi + '"'
-                  }
-                },
-                "filter": {
-                  "term": {"UPLOAD": 1}
-                }
-              }
-            }
-          }
-        }).then((resp) => {
-
-          //console.log('updatePrevious es', resp.hits.hits);
-
-          resp.hits.hits.forEach((hit) => {
-            c._summary.prev_version = hit._source.VERSION;
-            c._summary.prev_contributor_id = hit._source.CONTRIBUTOR_ID;
-            c._summary.prev_contributor = hit._source.CONTRIBUTOR;
-            c._summary.contribution.VERSION = '1';
-            if (c._summary.prev_version)
-              c._summary.contribution.VERSION = Number(c._summary.prev_version) + 1 + '';
-            c.contribution[0].version = c._summary.contribution.VERSION;
-            console.log('updatePrevious after', id, c._summary.contribution.VERSION, c.contribution[0].version);
-            updateContribution(id, c);
-            console.log('updatePrevious updated', Collections['magic.private.contributions'], id, c._summary.contribution.VERSION, c.contribution[0].version);
-
-          });
-
-        }, function (err) {
-          console.trace(err.message);
-        });
-      }
-    },
-    'updateDOI': function (id, doiData) {
+    async updateDOI(id, doiData) {
       check(id, String);
       check(doiData, Object);
 
@@ -297,31 +205,34 @@ export default function () {
       c._summary.contribution = c._summary.contribution || {};
       c.contribution = c.contribution || [{}];
 
-      if (doiData && doiData.title && doiData.title[0]) {
+      if (doiData.title && doiData.title[0]) {
         c._summary.contribution.TITLE = doiData.title[0];
       }
-      if (doiData && doiData['container-title'] && doiData['container-title'][0]) {
+
+      if (doiData['container-title'] && doiData['container-title'][0]) {
         c._summary.contribution.JOURNAL = doiData['container-title'][0];
       }
-      if (doiData && doiData.DOI)
-        c.contribution[0].doi = doiData.DOI;
-      if (doiData && doiData['published-print'] && doiData['published-print']['date-parts'] &&
+
+      if (doiData['published-print'] && doiData['published-print']['date-parts'] &&
         doiData['published-print']['date-parts'][0] && doiData['published-print']['date-parts'][0][0]) {
         c._summary.contribution.YEAR = doiData['published-print']['date-parts'][0][0];
       }
+
       c._summary.contribution.REFERENCE_KEYWORDS = doiData.subject.join(':') || undefined;
-      if (doiData && doiData.author && doiData.author.length == 1) {
+
+      if (doiData.author && doiData.author.length == 1) {
         c._summary.contribution.CITATION = doiData.author[0].family;
       }
-      if (doiData && doiData.author && doiData.author.length == 2) {
+      if (doiData.author && doiData.author.length == 2) {
         c._summary.contribution.CITATION = doiData.author[0].family + ' & ' + doiData.author[1].family;
       }
-      if (doiData && doiData.author && doiData.author.length > 2) {
+      if (doiData.author && doiData.author.length > 2) {
         c._summary.contribution.CITATION = doiData.author[0].family + ' et al.';
       }
       if (c._summary.contribution.YEAR)
         c._summary.contribution.CITATION += ' (' + c._summary.contribution.YEAR + ')';
       c._name = c._summary.contribution.CITATION;
+
       c._summary.contribution.REFERENCE_HTML = '<b>' +
         doiData.author.map((a) => a.family + ', ' + a.given).join(', ') +
         ' (' + c._summary.contribution.YEAR + ').</b> ' +
@@ -329,11 +240,43 @@ export default function () {
         c._summary.contribution.JOURNAL +
         (doiData.volume ? ' ' + doiData.volume : '') +
         (doiData.issue ? ' (' + doiData.issue + ')' : '') +
-        (doiData.page ? ':' + doiData.page : '') + '. ' +
-        'doi:<a href="//dx.doi.org/' + doiData.DOI + '">' + doiData.DOI + '</a>.</i>';
-      c.contribution[0].doi = doiData.DOI;
-      c._summary.contribution.DOI = doiData.DOI;
-      Collections['magic.private.contributions'].update(id, c);
+        (doiData.page ? ':' + doiData.page : '') + '.' +
+        (doiData.DOI ? ' doi:<a href="//dx.doi.org/' + doiData.DOI + '">' + doiData.DOI + '</a>.' : '') +
+        '</i>';
+
+      if (doiData.DOI) {
+        c.contribution[0].doi = doiData.DOI;
+        c._summary.contribution.DOI = doiData.DOI;
+        const resp = await esClient.search({
+          index: 'magic_v5', type: 'contributions_summaries',
+          body: {
+            "size": 1,
+            "sort": [{ "VERSION": "desc"}],
+            "query": {
+              "bool": {
+                "must": {
+                  "simple_query_string": {
+                    "query": '"' + doiData.DOI + '"'
+                  }
+                },
+                "filter": {
+                  "term": {"UPLOAD": 1}
+                }
+              }
+            }
+          }
+        });
+        if (resp.hits.hits.length > 0) {
+          const prev_summary = resp.hits.hits[0]._source;
+          c._summary.contribution.VERSION = Number(prev_summary.VERSION) + 1 + '';
+          c.contribution[0].version = Number(prev_summary.VERSION) + 1 + '';
+          c._summary.contribution.version_history = prev_summary.version_history;
+        }
+      }
+
+      await Collections['magic.private.contributions'].rawCollection().update({ _id: id}, c);
+      console.log('updatedDOI', c._summary.contribution.DOI);
+      return c;
     },
     'deleteContribution': function(id, user) {
       //console.log('deleting', id, user);
@@ -345,11 +288,16 @@ export default function () {
         }
       }
     },
-    'getContribution': function(id) {
-      //console.log('getting', id);
-      let c = Collections['magic.private.contributions'].findOne(id);
-      //console.log('got', _.keys(c));
-      return c;
+    'getPrivateContribution': function(id) {
+      const contributions = Collections['magic.private.contributions'].findOne(id);
+      return contributions;
+    },
+    'getPrivateContributions': function(contributor) {
+      const contributions = Collections['magic.private.contributions'].find(
+        {_contributor: contributor},
+        {sort: {'_inserted': -1}}
+      ).fetch();
+      return contributions;
     }
   });
 
