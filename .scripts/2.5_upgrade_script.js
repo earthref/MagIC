3d4432024984f8602cf360e5c532ac85823360d2
Rupert Minnett <rminnett@gmail.com>
2017-06-20 09:03:36 -0700

    Add upgrade scripts

--- .scripts/2.5_upgrade_script.js
+++ .scripts/2.5_upgrade_script.js
@@ -3,14 +3,20 @@ import fs from 'fs';
 import {expect} from 'chai';
 import _ from 'lodash';
 import Promise from 'bluebird';
+import elasticsearch from 'elasticsearch';
 import ParseContribution from '../parse_contribution';
 import UpgradeContribution from '../upgrade_contribution';
 import ExportContribution from '../export_contribution';
 import SummarizeContribution from '../summarize_contribution';
 
+const esClient = new elasticsearch.Client({
+  //log: 'trace',
+  host: "http://128.193.70.68:9200"
+});
+
 //var dirIn = 'D:/Google Drive/Cogense/Clients/Anthony Koppers/EarthRef/MagIC/Projects/Meteor/Upgrader/2.5 Contributions Test - Citations/';
 var dirIn = 'D:/Google Drive/Cogense/Clients/Anthony Koppers/EarthRef/MagIC/Projects/Meteor/Upgrader/2.5 Contributions/';
-var dirOut = 'client/modules/magic/actions/tests/output/upgraded2/';
+var dirOut = 'client/modules/magic/actions/tests/output/upgraded4/';
 if (!fs.existsSync(dirOut)) fs.mkdirSync(dirOut);
 
 describe('magic.actions.2_5_upgrade_script', () => {
@@ -23,7 +29,7 @@ describe('magic.actions.2_5_upgrade_script', () => {
     describe('when upgrading ' + file, function(file) {
       it('should process.', function (done) { setTimeout(() => {
         //if (fs.existsSync(dirOut + '/' + file + '.indexes/' + file + '.contribution.json')) {
-        if (fs.existsSync(dirOut + '/' + file + '.summary.json')) {
+        if (0 && fs.existsSync(dirOut + '/' + file + '.summary.json')) {
           done();
         } else {
           this.timeout(0);
@@ -34,14 +40,33 @@ describe('magic.actions.2_5_upgrade_script', () => {
           const parser = new ParseContribution({});
 
           parser.parsePromise({text: text}).then(() => {
-            //console.log('Parser output', parser.json);
-            try { expect(parser.errors().length).to.equal(0); } catch (err) { done(err); return }
+            console.log('Parser output', parser.json);
 
-            const upgrader = new UpgradeContribution({});
+            try {
+              expect(parser.errors().length).to.equal(0);
+              esByContribution(parser.json.contribution[0].id).then((resp) =>{
+                expect(resp.hits.total).to.be.below(2);
+                if (resp.hits.total === 1) {
+                  console.log(resp.hits.hits);
+                  fs.writeFileSync(dirOut + file, text);
+                }
+                done();
+              });
+
+            } catch (err) { done(err); return }
+
+
+
+
+
+
+
+            /*const upgrader = new UpgradeContribution({});
             upgrader.upgradePromise({json: parser.json}).then(() => {
               try { expect(upgrader.errors().length).to.equal(0); } catch (err) { done(err); return }
 
               const exporter = new ExportContribution({});
+              fs.writeFileSync(dirOut + file, text);
               fs.writeFileSync(dirOut+file+'.3.0.txt', exporter.toText(upgrader.json));
               fs.writeFileSync(dirOut+file+'.3.0.json', JSON.stringify(upgrader.json));
               try { expect(exporter.errors().length).to.equal(0); } catch (err) { done(err); return }
@@ -55,12 +80,12 @@ describe('magic.actions.2_5_upgrade_script', () => {
                     fs.writeFileSync(dirOut + '/' + file + '.indexes/' + file + '.' + key + '.' + idx + '.json', JSON.stringify(summarizer.json[key][val]));
                   });
                 });
-                fs.writeFileSync(dirOut + '/' + file + '.indexes/' + file + '.contribution.json', JSON.stringify(summarizer.json.contribution));*/
+                fs.writeFileSync(dirOut + '/' + file + '.indexes/' + file + '.contribution.json', JSON.stringify(summarizer.json.contribution));
                 try { expect(summarizer.errors().length).to.equal(0); } catch (err) { done(err); return }
                 done();
               });
 
-            });
+            });*/
 
           });
         }
@@ -71,3 +96,16 @@ describe('magic.actions.2_5_upgrade_script', () => {
 
   }
 });
+
+function esByContribution(id) {
+  return esClient.search({
+    index: 'magic_v5', type: 'contributions_summaries',
+    body: {
+      "query": {
+        "term": {
+          "MAGIC_CONTRIBUTION_ID": id
+        }
+      }
+    }
+  });
+}
