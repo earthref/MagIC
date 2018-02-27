b9676c87f3d2fff0244c2d81a4dd683a14642843
Nick Jarboe <njarboe@gmail.com>
2018-02-27 15:03:00 -0800

    Updated about and grand challenges pages with more text and images

--- client/modules/magic/components/contact.jsx
+++ client/modules/magic/components/contact.jsx
@@ -7,6 +7,7 @@ export default class extends React.Component {
     return (
       <div>
         <p>
+          <a href="#njarboe"></a>
           <b>Nick Jarboe</b> - <a href="mailto:njarboe@ucsd.edu">njarboe@ucsd.edu</a> - Data Analyst at the Scripps Institution of Oceanography, UC San Diego. <br/>
           Science community relations and outreach, data analyst, website content, MagIC workshops, PmagPy data integration.
         </p>
@@ -17,6 +18,7 @@ export default class extends React.Component {
           Nick Jarboe joined the MagIC team in 2011. He has a Ph.D. in Earth Science from UC Santa Cruz with a thesis on the behavior of the magnetic field during the Steens reversal (16.7 Ma) and the precise dating of the reversal using the <sup>40</sup>Ar/<sup>39</sup>Ar method on primarily plagioclase crystals from basalts. <a href="https://orcid.org/0000-0003-1465-9394" target="_blank">Website</a>
         </p>
         <p>
+          <a href="#rminnett"></a>
           <b>Rupert Minnett</b> - <a href="mailto:rminnett@earthref.org">rminnett@earthref.org</a> - Software Development Contractor, College of Earth, Ocean and Atmospheric Science, Oregon State University.<br/>
 	      Software developer for MagIC (Meteor, Elasticsearch, AWS, system administration), the MagIC API, custom data queries and updates.
         </p>
@@ -24,6 +26,7 @@ export default class extends React.Component {
           Rupert Minnett joined the MagIC team in 2005. He has a Ph.D. in computer engineering from UC San Diego with a thesis in biologically-inspired machine learning and computer vision. His primary interest is in enabling science with custom software solutions.
         </p>
         <p>
+          <a href="#ljonestrask"></a>
           <b>Lori Jonestrask</b> - <a href="mailto:ljonestrask@ucsd.edu">ljonestrask@ucsd.edu</a> - Software Developer at the Scripps Institution of Oceanography, UC San Diego. <br/>
 	      Software for PmagPy (Python, Windows and MacOS integration).
         </p>
@@ -39,13 +42,16 @@ export default class extends React.Component {
         </p>
         <p>
           <h3>Principal Investigators</h3>
+          <a href="#cconstable"></a>
           <b>Cathy Constable</b> - <a href="mailto:cconstable@ucsd.edu">cconstable@ucsd.edu</a> - Professor, Institute of Geophysics and Planetary Physics, Scripps Institution of Oceanography, UC San Diego.<br/>
           Research interests: paleomagnetism and geomagnetism, secular variation and reversals of the geomagnetic field, inverse problems, applications of statistical techniques in geophysics, electrical conductivity of the mantle, paleo and rock magnetic databases.<a href="http://scrippsscholars.ucsd.edu/cconstable" target="_blank">Website</a>
           <br/>
+          <a href="#akoppers"></a>
           <b>Anthony Koppers</b> - <a href="mailto:akoppers@ceoas.oregonstate.edu">akoppers@ceoas.oregonstate.edu</a> - Professor, College of Earth, Ocean and Atmospheric Sciences , Oregon State University.<br/> 
           Research interests: hotspots, tectonics, mantle geodynamics and plumes, <sup>40</sup>Ar/<sup>39</sup>Ar geochronology, online data 
           curation (<a href="https://earthref.org">earthref.org</a>, <Link to="/MagIC">MagIC</Link>), isotope geochemistry. <a href="http://ceoas.oregonstate.edu/profile/koppers/" target="_blank">Website</a>
           <br/>
+          <a href="#ltauxe"></a>
           <b>Lisa Tauxe</b> - <a href="mailto:ltauxe@ucsd.edu">ltauxe@ucsd.edu</a> - Professor, Geoscience Research Divison, Scripps Institution of Oceanography, UC San Diego.<br/> 
           Research interests: paleomagnetism, rock magnetism, magnetostratigraphy, paleointensity, archaeomagnetism, ancient geomagnetic field. <a href="http://scrippsscholars.ucsd.edu/ltauxe" target="_blank">Website</a>
 		    </p>
