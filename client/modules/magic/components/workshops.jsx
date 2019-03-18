344d9b0006bed78e0076f279b4de2991c9db43e6
Rupert <rminnett@gmail.com>
2019-03-17 22:53:02 -0700

    Change next workshop year to 2020

--- client/modules/magic/components/workshops.jsx
+++ client/modules/magic/components/workshops.jsx
@@ -10,8 +10,8 @@ export default class extends React.Component {
          MagIC has hosted workshops in 2011, 2014 and 2017 at the Scripps Institution of Oceanography in La Jolla, California. The format is generally two days of science talks followed by a few days of hands-on tutorials. During the tutorials we give presentations working on uploading data into the MagIC database and using the PmagPy paleomagnetic software suite. There is plenty of time given to one-on-one interaction with the MagIC team for help or to give us suggestions. <br/>
         </p>
         <p>
-         <b>2019 MagIC Workshop</b><br/>
-           We plan to host another workshop in 2019. We like to have at least one day of talks on a related theme. If you have suggestions for the workshop, please feel free to <Link style={{color: 'purple'}} to={'contact'}>email us</Link> with suggestions.<br/>
+         <b>2020 MagIC Workshop</b><br/>
+           We plan to host another workshop in 2020. We like to have at least one day of talks on a related theme. If you have suggestions for the workshop, please feel free to <Link style={{color: 'purple'}} to={'contact'}>email us</Link> with suggestions.<br/>
         </p>
         <p>
          <b>2017 MagIC Workshop</b><br/>
