5b73dd57a3cb31dbc4908f6664630ed411b60cfe
Rupert Minnett <rminnett@gmail.com>
2019-11-26 10:30:52 -0800

    Use DataCite instead of Crossref metadata with EZID

--- tests/data_doi.tests.js
+++ tests/data_doi.tests.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
 import elasticsearch from "elasticsearch";
 import BPromise from 'bluebird';
@@ -7,6 +8,7 @@ const esClient = new elasticsearch.Client({
   //log: "trace",
   host: "http://128.193.70.68:9200",
   keepAlive: false,
+  apiVersion: '6.8',
   requestTimeout: 60 * 60 * 1000 // 1 hour
 });
 
@@ -23,9 +25,18 @@ let index = "magic_v4";
         index: index, type: "contribution", size: 1e4, 
         _source: [
           "summary.contribution.id", 
+          "summary.contribution.timestamp", 
+          "summary.contribution.version", 
           "summary.contribution._reference.title", 
           "summary.contribution._reference.long_authors", 
-          "summary.contribution._reference.year"
+          "summary.contribution._reference.year", 
+          "summary.contribution._reference.journal", 
+          "summary.contribution._reference.doi", 
+          "summary.contribution._reference.authors", 
+          "summary._all.geologic_classes", 
+          "summary._all.geologic_types", 
+          "summary._all.lithologies", 
+          "summary._all._geo_envelope"
         ],
         body: {
           "query": { "bool": { 
@@ -33,10 +44,12 @@ let index = "magic_v4";
               { "exists": { "field": "summary.contribution._reference.long_authors" }},
               { "exists": { "field": "summary.contribution._reference.title" }},
               { "exists": { "field": "summary.contribution._reference.year" }},
-              { "term": { "summary.contribution._is_latest": "true"}}
+              { "exists": { "field": "summary.contribution.version" }},
+              { "term": { "summary.contribution._is_activated": "true"}},
+              //{ "term": { "summary.contribution._is_latest": "true"}}
             ],
-            "must_not": [{ "term": { "summary.contribution._has_data_doi": "true"}}]
-            //"filter": { "term": { "summary.contribution.id": 16550}}
+            //"must_not": [{ "term": { "summary.contribution._has_data_doi": "true"}}]
+            "filter": { "term": { "summary.contribution.id": 16515}}
           }}
         }
       }).then((resp) => {
@@ -45,41 +58,132 @@ let index = "magic_v4";
 
           BPromise.each(resp.hits.hits, hit => {
             return new Promise((resolve) => {
-              let xml = `<?xml version="1.0"?>
-<database xmlns="http://www.crossref.org/schema/4.4.0"
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xsi:schemaLocation="http://www.crossref.org/schema/4.4.0
-  http://www.crossref.org/schema/deposit/crossref4.4.0.xsd"
->
-  <contributors>
-    <person_name contributor_role="author" sequence="first">
-      <surname>${hit._source.summary.contribution._reference.long_authors}</surname>
-    </person_name>
-  </contributors>
-  <titles>
-    <title>${hit._source.summary.contribution._reference.title}</title>
-  </titles>
-  <publisher_item>
-    <item_number item_number_type="MagIC Contribution ID">${hit._source.summary.contribution.id}</item_number>
-  </publisher_item>
-  <publisher><publisher_name>Magnetics Information Consortium (MagIC)</publisher_name></publisher>
-  <publication_date><year>${hit._source.summary.contribution._reference.year}</year></publication_date>
-  <doi_data>
-    <doi></doi>
-    <resource></resource>
-  </doi_data>
-</database>`;
-              xml = xml.replace(/%/g, "%25").replace(/\s*\n\s*/g, "%0A").replace(/\r/g, "%0D").replace(/:/g, "%3A");
+
+              let related = hit._source.summary.contribution._reference.doi &&
+                `<relatedIdentifiers>
+                  <relatedIdentifier relatedIdentifierType="DOI" relationType="IsDocumentedBy">` +
+                    `${hit._source.summary.contribution._reference.doi.replace(/</g, "&lt;").replace(/>/g, "&gt;")}` +
+                  `</relatedIdentifier>
+                </relatedIdentifiers>`;
+              let subjects = [];
+              hit._source.summary._all && hit._source.summary._all.geologic_classes && 
+                hit._source.summary._all.geologic_classes.forEach(geologic_class => {
+                  subjects.push(
+                    `<subject subjectScheme="earthref.magic.geologic_classes">${geologic_class}</subject>`
+                  );
+                });
+              hit._source.summary._all && hit._source.summary._all.geologic_types && 
+                hit._source.summary._all.geologic_types.forEach(geologic_type => {
+                  subjects.push(
+                    `<subject subjectScheme="earthref.magic.geologic_types">${geologic_type}</subject>`
+                  );
+                });
+              hit._source.summary._all && hit._source.summary._all.lithologies && 
+                hit._source.summary._all.lithologies.forEach(lithology => {
+                  subjects.push(
+                    `<subject subjectScheme="earthref.magic.lithologies">${lithology}</subject>`
+                  );
+                });
+              subjects = subjects.length && `<subjects>${subjects.join('')}</subjects>`;
+
+              let geos = [];
+              hit._source.summary._all && hit._source.summary._all._geo_envelope && _.sortedUniqBy(
+                _.sortBy(hit._source.summary._all._geo_envelope, 
+                  x => _.flatten(x.coordinates).join('_')), 
+                x => _.flatten(x.coordinates).join('_'))
+              .forEach((envelope, i) => {
+                let n = geos.push('') - 1;
+                geos[n] += '<geoLocation>';
+                if (
+                  envelope.coordinates[0][0] == envelope.coordinates[1][0] &&
+                  envelope.coordinates[0][1] == envelope.coordinates[1][1]
+                ) {
+                  geos[n] += 
+                    `<geoLocationPoint>
+                      <pointLongitude>${envelope.coordinates[0][0]}</pointLongitude>
+                      <pointLatitude>${envelope.coordinates[0][1]}</pointLatitude>
+                    </geoLocationPoint>`;
+                } else {
+                  geos[n] += 
+                    `<geoLocationBox>
+                      <westBoundLongitude>${envelope.coordinates[0][0]}</westBoundLongitude>
+                      <eastBoundLongitude>${envelope.coordinates[1][0]}</eastBoundLongitude>
+                      <southBoundLatitude> ${envelope.coordinates[0][1]}</southBoundLatitude>
+                      <northBoundLatitude>${envelope.coordinates[1][1]}</northBoundLatitude>
+                    </geoLocationBox>`;
+                }
+                geos[n] += '</geoLocation>';
+              });
+              geos = geos.length && `<geoLocations>${geos.join('')}</geoLocations>`;
+
+              let creators = [];
+              hit._source.summary.contribution._reference.authors &&
+                hit._source.summary.contribution._reference.authors.forEach(author => {
+                  creators.push(
+                    `<creator>
+                      ${(author.given || author.family) && (
+                        `<creatorName>` +
+                          `${author.given}${author.given && author.family && ' '}${author.family}` +
+                        `</creatorName>`) || '' }
+                      ${author._orcid && (
+                        `<nameIdentifier schemeURI="https://orcid.org" nameIdentifierScheme="ORCID">` +
+                          `${author._orcid}` +
+                        `</nameIdentifier>`) || '' }
+                      ${author.affiliation && author.affiliation.map(affiliation =>
+                        `<affiliation>${affiliation}</affiliation>`).join('') || '' }
+                    </creator>`
+                  );
+                });
+              creators = creators.length && `<creators>${creators.join('')}</creators>` || 
+                `<creators><creator><creatorName>` +
+                  `${hit._source.summary.contribution._reference.long_authors}` +
+                `</creatorName></creator></creators>`;
+
+              let datacite = 
+                `<?xml version="1.0"?>
+                <resource 
+                  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                  xmlns="http://datacite.org/schema/kernel-4"
+                  xsi:schemaLocation="http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4.3/metadata.xsd"
+                >
+                  <identifier identifierType="DOI">10.7288/V4/MAGIC/${hit._source.summary.contribution.id}</identifier>
+                  ${creators}
+                  <titles>
+                    <title>${hit._source.summary.contribution._reference.title} (Dataset)</title>
+                  </titles>
+                  <contributors>
+                    <contributor contributorType="Distributor">
+                      <contributorName>Magnetics Information Consortium (MagIC)</contributorName>
+                    </contributor>
+                  </contributors>
+                  <publisher>${hit._source.summary.contribution._reference.journal || 
+                    'Magnetics Information Consortium (MagIC)'}</publisher>
+                  <publicationYear>${hit._source.summary.contribution._reference.year}</publicationYear>
+                  <version>${hit._source.summary.contribution.version}</version>
+                  <dates>
+                    <date dateType="Available">${hit._source.summary.contribution.timestamp.substr(0, 10)}</date>
+                  </dates>
+                  <language>en-US</language>
+                  <resourceType resourceTypeGeneral="Dataset"/>
+                  ${related || ''}
+                  ${geos || ''}
+                  ${subjects || ''}
+                  <rightsList>
+                    <rights rightsURI="http://creativecommons.org/publicdomain/zero/1.0/"/>
+                  </rightsList>
+                </resource>`;
+              // console.log(datacite);
+              datacite = datacite.replace(/%/g, "%25").replace(/\s*\n\s*/g, "%0A").replace(/\r/g, "%0D").replace(/:/g, "%3A");
               let payload =
-`_profile: crossref
-_status: public
-_target: https://earthref.org/MagIC/${hit._source.summary.contribution.id}
-crossref: ${xml}`;
+                `_profile: datacite\n` +
+                `_status: public\n` +
+                `_target: https://earthref.org/MagIC/${hit._source.summary.contribution.id}\n` +
+                `datacite: ${datacite}`;
 
               //console.log('PUT', 'https://ezid.cdlib.org/id/doi:10.7288/V4/MAGIC/' + hit._source.summary.contribution.id, payload);
               request({
                 method: 'PUT',
-                uri: 'https://ezid.cdlib.org/id/doi:10.7288/V4/MAGIC/' + hit._source.summary.contribution.id,
+                uri: 'https://ezid.cdlib.org/id/doi:10.7288/V4/MAGIC/' + hit._source.summary.contribution.id + '?update_if_exists=yes',
                 auth: Meteor.settings.ezid,
                 headers: {'content-type': 'text/plain; charset=UTF-8'},
                 body: payload
@@ -107,7 +211,7 @@ crossref: ${xml}`;
                     resolve();
                   });
                 }  else if (/^error: /.test(body)) {
-                  console.error(body);
+                  console.error(hit._id, body);
                   resolve();
                 } else {
                   esClient.update({
