8f27d88b751e6e314fe6bebc45fa9e335d26a2c6
Nick Jarboe <njarboe@gmail.com>
2020-05-04 15:57:06 -0700

    changed workshop news, added tutorial news

--- client/modules/magic/components/home_news.jsx
+++ client/modules/magic/components/home_news.jsx
@@ -11,24 +11,49 @@ export default class extends React.Component {
       <div style={{textAlign: "justify"}}>
         <h3>
           <Image size="mini" src="/MagIC/sio.jpg" floated="left"/>
-          {` 2020 MagIC Workshop`}
+          {` MagIC Workshop 2021`}
         </h3>
-        <Message color="red" style={{ marginTop: '1.5em' }}>
-          {' The 2020 MagIC Workshop has been postponed to 20-22 January 2021 due to the COVID-19 outbreak. '}
-        </Message>
         <p>
-          {` MagIC is hosting the `}
-          <a href="https://earthref.org/events/MAGIC/2020/"><b>2020 Workshop: Rock and Paleomagnetism through Time and Space</b></a>
-          {` on `}
-          <b>March 16th-18th, 2020</b>
-          {` at SIO in La Jolla, CA. It consists of two days of `} <b>science talks</b>
-          {` followed by a day of hands-on tutorials, during which participants will work on `}
+          {` MagIC is planning to host it's postponed 2020 workshop on `}
+          <b>January 20th-22nd, 2021</b>
+          {`. The retitled workshop, `}
+          <b>MagIC Workshop 2021: Rock and Paleomagnetism through Time and Space</b>
+          {`, will be held at SIO in La Jolla, CA. It will consist of two days of `}
+          <b>science talks</b>
+          {` followed by a day of `}
+          <b>hands-on tutorials</b>
+          {`, during which participants will work on `}
           <Link to="/MagIC/upload"><b>uploading</b></Link>
           {` data into their `}
           <Link to="/MagIC/private"><b>private workspace</b></Link>
           {` for contribution to the MagIC database and practice using the `}
           <a href="https://earthref.org/PmagPy/cookbook/"><b>PmagPy</b></a>
           {` paleomagnetic software suite.`}
+          </p>
+          <p>
+          {`Due to the ongoing COVID-19 outbreak, this date and/or live attendance is still `}
+          <b>preliminary</b>
+          {`. Please check back here for the most recent information. We will also send updates about the workshop to the gpmag list, the MagIC user list, and to the cancelled workshop registrants.`}
+        </p>
+        <div className="ui divider"></div>
+        <h3>
+          <Image size="mini" src="/MagIC/youtube.png" floated="left"/>
+          <a href="https://www.youtube.com/playlist?list=PLirL2unikKCgUkHQ3m8nT29tMCJNBj4kj"><font color="000000">2020 MagIC Workshop Tutorial Videos</font></a>
+        </h3>
+        <p>
+        {`In lieu of the postponed 2020 MagIC Workshop, Magic hosted a series of `}
+        <a href="https://www.youtube.com/playlist?list=PLirL2unikKCgUkHQ3m8nT29tMCJNBj4kj"><b>tutorials</b></a>
+        {` over three days covering some of the material which was to be presented in the hands-on part of the workshop. The `}
+        <a href="https://youtu.be/pchdPBzSmT0"><b>first</b></a> 
+        {` was on the MagIC data model and the uploading of data to the MagIC database, the `}
+        <a href="https://youtu.be/GUjf33aNnFQ"><b>second</b></a>
+        {` was on the PmagPy Demag GUI data import, analysis, and MagIC file export software, the `}
+        <a href="https://www.youtube.com/watch?v=9yGPbATqRtI"><b>third</b></a>
+        {` was on using PmagPy and Jupyter notebooks. These tutorial videos and others can be found on the `}
+        <a href="https://www.youtube.com/playlist?list=PLirL2unikKCgUkHQ3m8nT29tMCJNBj4kj"><b>2020 MagIC Workshop Tutorial playlist</b></a>
+        {` on the `}
+        <a href="https://www.youtube.com/channel/UC-DbvhEu49a6dZXdvUWorhQ"><b>MagIC YouTube Channel</b></a>
+        {`.`}
         </p>
         <div className="ui divider"></div>
         <h3>
