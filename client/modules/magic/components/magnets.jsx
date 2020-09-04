ae1f50c7653d2e07f7a60bae2fbdebdde2b26d0d
Rupert <rminnett@gmail.com>
2020-09-03 22:16:27 -0400

    Add MagNetS markdown fetched from GitHub

--- client/modules/magic/components/magnets.jsx
+++ client/modules/magic/components/magnets.jsx
@@ -1,39 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Markdown from 'markdown-to-jsx';
 
-import {Link} from 'react-router-dom';
+export default () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const resp = await fetch('https://cdn.jsdelivr.net/gh/earthref/MagIC-MagNetS/magnets.md');
+      setData(resp.ok && await resp.text());
+    }
+    fetchData();
+  }, []);
 
-export default class extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <p>
-        MagNetS is a bi-weekly on-line seminar covering topics in the geomagnetism, paleomagnetism, rock magnetism, and archeomagnetism.<br/>
-        </p>
-        <p>
-         <b>Next Seminar</b><br/>
-         "Recurring anomalous magnetic field behavior in the South Atlantic on multi-million year timescales"<br/>
-         Author: <b>Yael Engbers</b> (University of Liverpool)<br/>
-         Author Website: <a href=""></a><br/>
-         MagIC contribution: <a href="https://earthref.org/MagIC/doi/10.1073/PNAS.2001217117">earthref.org/MagIC/doi/10.1073/PNAS.2001217117</a><br/>
-        </p>
-        <p>
-         <b>Previous Seminars</b><br/>
-
-         "Landscapes on Fire: Do rocks baked by coal seam fires reliably record the geomagnetic field?"<br/>
-         Author: <b>Courtney Sprain</b> (University of Florida)<br/>
-         Author Website: <a href="https://geology.ufl.edu/people/faculty/courtney-sprain/">geology.ufl.edu/people/faculty/courtney-sprain</a><br/>
-         Download talk at: <a href="https://earthref.org/ERDA/2443/">earthref.org/ERDA/2443</a><br/>
-         YouTube Video: <a href="https://youtu.be/Opf5qT4zujA">youtu.be/Opf5qT4zujA</a><br/>
-         Presentation Reference DOI: 10.7288/V4/ERDA/2442<br/>
-        </p>
-        <p>
-         <b>Contact</b><br/>
-         Please send questions, interest in giving a seminar, or a request to be put on the mailing list for the seminars to <a href='mailto:greig.paterson@liverpool.ac.uk'><font color='purple'>greig.paterson@liverpool.ac.uk</font></a><br/>  
-        </p>
+  return (<div>
+    { data === null &&
+      <div className="ui segment" style={{minHeight: "8em"}}>
+        <div className="ui inverted active dimmer">
+          <div className="ui text loader">Loading</div>
+        </div>
       </div>
-    );
-  }
-
+    }
+    { data === false &&
+      <div className="ui error huge message" style={{textAlign: "center"}}>
+        Error Loading Content!
+      </div>
+    }
+    { data &&
+      <Markdown>{data}</Markdown>
+    }
+  </div>);
 }
-
