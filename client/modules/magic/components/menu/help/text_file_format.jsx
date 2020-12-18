f924a607a7e0930cec4721f3bce995e91804be44
Nick Jarboe <njarboe@gmail.com>
2020-12-17 19:02:54 -0800

    improved text file format help and controlled vocab label change

--- client/modules/magic/components/menu/help/text_file_format.jsx
+++ client/modules/magic/components/menu/help/text_file_format.jsx
@@ -9,7 +9,10 @@ export default class extends React.Component {
       <div>
         <h4>MagIC Standard Format</h4>
         <p>
-         The MagIC text file format is a tab delimited file which contains the data for a contribution archived in the MagIC datastore. Empty tables and columns are not placed in the MagIC text file. The format consists of a table name header (the word "delimited" is optional), column names, data, and a table separator. Please see the <Link style={{color: 'purple'}} to={'/MagIC/data-models/3.0'}>MagIC data model</Link> for a detailed description of all the possible tables and columns available in the MagIC datastore.<br/> 
+         The MagIC text file format is a tab delimited file which contains the data for a contribution archived 
+         in the MagIC datastore. Empty tables and columns are not placed in the MagIC text file. The format consists
+         of a table name header (the word "delimited" is optional), column names, data, and a table separator. 
+         Please see the <Link style={{color: 'purple'}} to={'/MagIC/data-models/3.0'}>MagIC data model</Link> for a detailed description of all the possible tables and columns available in the MagIC datastore.<br/> 
         </p>
         <p>
 An example of a MagIC text file that contains contribution, location, and site level information (-- represents tab) is shown below. The contribution table is not generally used while uploading and is created by the MagIC data upload software.
@@ -47,9 +50,10 @@ E28--Eastern Sheep Creek--GM-ARAR:LT-AF-Z:SO-SM:LP-DIR-AF:FS-FD--This study--Ext
           For large MagIC upload files with columns that repeat the same values for every row, a compact form of the MagIC file format should be used. This allows for a large reduction in size in the measurement table for MagIC files with XPEEM, SQUID microscopy, FORC, QDM and other image like data. Large MagIC sites tables due to long core records can also often be reduced significantly in size by using the compact format.
         </p>
         <p>
-          The format consists of placing the repeated column values at the top of a table and then putting the rest of the rest of the table below that as usual. Places a "*", then a space, then the column name, then a tab, and then the column value that is repeated for every row. For columns with multiple values, one value is allowed to be substituted while the rest of the row remains the same. This is done by placing a "*" in the compact column value and then putting the changing row values in the regular part of the table (see the derived_value column below for an example).
+          The format consists of placing the repeated column values at the top of a table and then putting the rest of the rest of the table below that as usual. Place a "*", then a space, then the column name, then a tab (shown as --), and then the column value that is repeated for every row. For columns with multiple values, one value is allowed to be substituted while the rest of the row remains the same. This is done by placing a "*" in the compact column value and then putting the changing row values in the regular part of the table. See the derived_value column below for an example. In this case this reduces the file size significantly because the text "XPEEM," and ",10.1088/1742-6596/430/1/012127" only appear in the data header and not in thousands of rows.
         </p>
         <p>
+        <b>X-ray photoemission electron microscopy (XPEEM) file example from <a href="https://earthref.org/MagIC/16837">Maurel et al. (2020)</a></b><br/>
         <code>
            tab--measurements<br/>
            * experiment--CoA01-r1offR<br/>
@@ -63,15 +67,30 @@ E28--Eastern Sheep Creek--GM-ARAR:LT-AF-Z:SO-SM:LP-DIR-AF:FS-FD--This study--Ext
            1--41--0.0--0.0<br/>
            2--43--9.488e-09--0.0<br/>
            3--49--1.8976e-08--0.0<br/>
-          </code>
+        </code>
         <br/>
-        <b>SQUID microscopy file example</b><br/>
-        For an example of a SQUID microscopy study using the compact MagIC file format 
-        see <a href="https://earthref.org/MagIC/16969">Borlina et al. (2020)</a>.
+        <b>SQUID Microscopy (SM) file example from <a href="earthref.org/MagIC/17039">Borlina et al. (2020)</a></b><br/>
+        <code> 
+           tab--measurements<br/>
+           * experiment--    F1_115C_Z_150107_h18_map1<br/>
+           * specimen--      150107_h18<br/>
+           * standard--      u<br/>
+           * quality--       g<br/>
+           * method_codes--  LP-SQUIDM<br/>
+           * citations--     This study<br/>
+           * description--   Scanning Direction: Y, Scanning Mode: Continuous, Scanning Speed: 3000 m/s, Grid Size: 121 x 121, Number of points: 14641, Sampling Mode: Single-point, Sampling Frequency: N/A, Calibration Factor: 1.000 nT/V, Scan Start Time: 10:04:14 PM on 11/10/2017, Scan End Time: 10:12:12 PM on 11/10/2017, Total Scanning Time: 0 hours, 8 minutes.<br/>
+           * derived_value SQUID Microscopy Model Height Lima And Weiss 2016,0.000429,10.1002/2016GC006487;SQUID Microscopy Residuals Lima And Weiss 2016,0.4214,10.1002/2016GC006487<br/>
+           measurement--magn_z--meas_pos_x--meas_pos_y<br/>
+           1--4.860960133333341e-12--4.7e-07--6.95e-07<br/>
+           2--4.4558579999999895e-12--2.5469999999999995e-05--6.95e-07<br/>
+           3---8.101379e-13--5.046999999999999e-05--6.95e-07<br/>
+           4--8.50661386666666e-12--7.546999999999998e-05--6.95e-07<br/>
+           5--8.102042666666771e-13--0.00010047--6.95e-07<br/>
+        </code> 
         </p>
         <h4>MagIC Zip File Download Formats</h4>
         <p>
-            If the MagIC test files are very large, the data may be configured as a zip file for downloading. This zip file may contain just one MagIC text file or may contain multiple files where the measurement data has been divided into separate files for each experiment. This separation is useful when the individual MagIC file size would be many gigabytes.
+            If the MagIC test files are very large, the data may be configured as a zip file for downloading. This zip file may contain just one MagIC text file or may contain multiple files where the measurement data has been divided into separate files for each experiment. This separation is useful when the individual MagIC file size would be many gigabytes and unwieldy to work with.
         </p>
       </div>
     );
