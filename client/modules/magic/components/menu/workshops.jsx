09f3d1405a288fd017c9cf68409f6e2c8b565a26
Nick Jarboe <njarboe@gmail.com>
2020-12-18 17:12:58 -0800

    added 2021 magic workshop

--- client/modules/magic/components/menu/workshops.jsx
+++ client/modules/magic/components/menu/workshops.jsx
@@ -10,14 +10,29 @@ export default class extends React.Component {
          MagIC has hosted workshops in 2011, 2014 and 2017 at the Scripps Institution of Oceanography in La Jolla, California. The format is generally two days of science talks followed by a few days of hands-on tutorials. During the tutorials we give presentations about uploading data into the MagIC database and installing, using, and contributing to the PmagPy paleomagnetic software suite. There is plenty of time given to one-on-one interaction with the MagIC team for help or to give us suggestions. In 2020 the MagIC in-person workshop was postponed due the SARS-CoV-2 virus outbreak and was replaced by a shortened virtual meeting.<br/>
         </p>
         <p>
+         <b>2021 MagIC Workshop</b><br/>
+         The 2021 MagIC Workshop will be held virtually from Jan 19th through Jan 21st, 2021.
+         It will consist of four live scientific sessions covering a range of topics in rock, geo,
+         and paleomagnetism. We also have a presentation of MagIC's progress and future plans.
+         To accommodate participants of various time zones, we have scheduled one 
+         session in the evening (PST). Please visit the 
+         workshop <a href="https://earthref.org/events/MAGIC/2021/"><b>website</b></a> for the talk schedule and 
+         other information.
+        </p>
+        <p>
+         Registration is free and we have a Slack workspace where the workshop participants can 
+         interact, comment on talks, and download posters. You can register for the workshop by filling out this <a href="https://docs.google.com/forms/d/118u4mwEQHny_r1KsDMJ9aXllacJHqRziIAmcgZEXSBs"><b>form</b></a>.
+         Everyone is welcome to present a poster.
+        </p>
+        <p>
          <b>2020 MagIC Workshop</b><br/>
-           The 2020 MagIC workshop was to take place March 16th-18th, 2020 in La Jolla, California at the Scripps Institution of Oceanography. Due to the SARS-CoV-2 virus outbreak the in-person workshop has been postponed into 2021. Instead, a virtual meeting was held with one speaker each day giving a talk about MagIC technology and tools. Those talks were recorded and can be viewed on the MagIC YouTube channel: <a href="https://www.youtube.com/playlist?list=PLirL2unikKCgUkHQ3m8nT29tMCJNBj4kj">2020 MagIC Workshop Tutorials</a>.<br/>
+           The 2020 MagIC workshop was to take place March 16th-18th, 2020 in La Jolla, California at the Scripps Institution of Oceanography. Due to the SARS-CoV-2 virus outbreak the in-person workshop has been postponed into 2021. Instead, a virtual meeting was held with one speaker each day giving a talk about MagIC technology and tools. Those talks were recorded and can be viewed on the MagIC YouTube channel: <a href="https://www.youtube.com/playlist?list=PLirL2unikKCgUkHQ3m8nT29tMCJNBj4kj"><b>2020 MagIC Workshop Tutorials</b></a>.<br/>
         </p>
         <p>
          <b>2017 MagIC Workshop</b><br/>
          For a detailed description, schedule, and list of participants of the 2017 workshop, you can visit
-         its <a href='https://earthref.org/events/MAGIC/2017/'>homepage</a>.<br/>
-         Videos of many of the talks can be found on our <a href='https://www.youtube.com/channel/UC-DbvhEu49a6dZXdvUWorhQ'>YouTube channel</a>.
+         its <a href='https://earthref.org/events/MAGIC/2017/'><b>homepage</b></a>.<br/>
+         Videos of many of the talks can be found on our <a href='https://www.youtube.com/channel/UC-DbvhEu49a6dZXdvUWorhQ'><b>YouTube channel</b></a>.
         </p>
       </div>
     );
