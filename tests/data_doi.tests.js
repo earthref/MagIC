4467fe7076b879f520732e5125145b843e359be5
Rupert Minnett <rminnett@Gmail.com>
2022-01-24 00:19:21 -0600

    Use settings for elasticsearch

--- tests/data_doi.tests.js
+++ tests/data_doi.tests.js
@@ -6,7 +6,7 @@ import request from 'request';
 
 const esClient = new elasticsearch.Client({
   //log: "trace",
-  host: "http://128.193.70.68:9200",
+  host: Meteor.settings.elasticsearch.url,
   keepAlive: false,
   apiVersion: '6.8',
   requestTimeout: 60 * 60 * 1000 // 1 hour
