d77c394e8f064f5869e50976b216e860572773e5
Nick Jarboe <njarboe@gmail.com>
2022-10-05 13:26:48 -0600

    Updated and added links

--- client/modules/magic/components/menu/links.jsx
+++ client/modules/magic/components/menu/links.jsx
@@ -13,15 +13,16 @@ export default class extends React.Component {
          <a href="http://geomagia.gfz-potsdam.de/">GEOMAGIA50</a> - A paleomagnetic and chronological database for the past 50 ka - Maxwell Brown<br/>   
          {/* <a href="http://www.ngdc.noaa.gov/geomag/paleo.shtml">IAGA geomagnetic databases</a> - compiled in the late 90' early 2000's (<a href="https://www.ngdc.noaa.gov/geomag/data/paleo/gpmdb_46.zip" >GPMDB 4.6</a>, <a href="ftp://ftp.ngdc.noaa.gov/geomag/Paleomag/access/ver3.5">Archeo</a>, <a href="ftp://ftp.ngdc.noaa.gov/geomag/Paleomag/access/ver3.5">Magst</a>, <a href="ftp://ftp.ngdc.noaa.gov/geomag/Paleomag/access/ver3.5">Pint</a>, <a href="ftp://ftp.ngdc.noaa.gov/geomag/Paleomag/access/ver4.1">PSVRL</a>, <a href="ftp://ftp.ngdc.noaa.gov/geomag/Paleomag/access/ver3.5">Secvr</a>, <a href="ftp://ftp.ngdc.noaa.gov/geomag/Paleomag/access/ver3.5">Trans</a>)<br/> */}
          <a href="http://www.ngdc.noaa.gov/geomag/paleo.shtml">IAGA geomagnetic databases</a> - databases compiled in the late 90' early 2000's (GPMDB 4.6, Archeo, Magst, PintPSVRL, Secvr, and Trans)<br/>
-         <a href="http://h175.it.helsinki.fi/database/">PALEOMAGIA</a> - Precambrian paleomagnetic pole database - Toni Veikkolainen<br/>
-         <a href="http://earth.liv.ac.uk/pint/">PINT</a> - Absolute paleointensity database - Andy Biggin<br/>   
+         <a href="https://paleomagia.it.helsinki.fi/">PALEOMAGIA</a> - Precambrian paleomagnetic pole database - Toni Veikkolainen<br/>
+         <a href="http://www.pintdb.org/">PINT</a> - Absolute paleointensity database - Andy Biggin<br/>   
+         <a href="http://icepmag.org/">ICEPMAG</a> - Iceland Palaeomagnetism Database - Justin Tonti-Filippini and Maxwell Brown<br/>   
         </p>
         <p>
          <b>Software Tools</b><br/>
          <a href="https://wserv4.esc.cam.ac.uk/nanopaleomag/?page_id=31">FORCinel</a> - Calculates first order reversal curve (FORC) diagrams from raw FORC data files - Richard Harrison<br/>
-         <a href="https://earthref.org/PmagPy/cookbook/">PmagPy</a> - Open source Paleo and rock magnetic anaysis software for use on the command line, in Jupyter notebooks, or through stand alone GUIs. - Lisa Tauxe and others<br/>
-         <a href="https://www.geophysik.uni-muenchen.de/research/magnetism/software">ThellierTool</a> - Roman Leonhardt<br/>
-         <a href="https://wserv4.esc.cam.ac.uk/nanopaleomag/?page_id=193">VARIFORC</a> - Additional FORC diagram visualization tool. Starts with FORCinel files. - Ramon Egli<br/>
+         <a href="https://earthref.org/PmagPy/cookbook/">PmagPy</a> - Open source Paleo and rock magnetic anaysis software for use on the command line, in Jupyter notebooks, or through stand alone GUIs - Lisa Tauxe and others<br/>
+         <a href="https://www.geophysik.uni-muenchen.de/en/research/magnetism/software">ThellierTool and RockMag Analyzer</a> - Roman Leonhardt<br/>
+         <a href="https://wserv4.esc.cam.ac.uk/nanopaleomag/?page_id=193">VARIFORC</a> - Additional FORC diagram visualization tool. Starts with FORCinel files - Ramon Egli<br/>
         </p>
         <p>
          <b>Organizations</b><br/>
@@ -36,6 +37,4 @@ export default class extends React.Component {
       </div>
     );
   }
-
 }
-
