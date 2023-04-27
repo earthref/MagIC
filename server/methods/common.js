f0b872197ae4888421a22999f60eeb35bb9aeebf
Rupert Minnett <rminnett@gmail.com>
2023-04-26 22:42:44 -0600

    Fallback on Datacite reference metadata

--- server/methods/common.js
+++ server/methods/common.js
@@ -131,88 +131,173 @@ export default function () {
       return __.omitDeep(user, /(^|\.)_/);
     },
 
-    async getReferenceMetadata(doi, attempt = 0) {
-      this.unblock();
-      // console.log("getReferenceMetadata", attempt, `http://api.crossref.org/works/${doi}`);
-      let resp = HTTP.call("GET", `http://api.crossref.org/works/${doi}`);
-      if (!resp.statusCode || !resp.data || !resp.data.message) {
-        if (attempt < 3) {
-          return Meteor.call("getReferenceMetadata", doi, attempt + 1);
-        } else {
-          console.error("getReferenceMetadata", `Failed to retrieve reference metadata for doi ${doi}`);
-          throw new Meteor.Error("getReferenceMetadata", `Failed to retrieve reference metadata for doi ${doi}`);
-        }
-      } else {
-        let d = resp.data.message;
-        let _reference = {
-          source: 'crossref',
-          doi: _.toUpper(d.DOI)
-        };
-
-        if (d.title && d.title.length > 0)
-          _reference.title = d.title[0];
-
-        if (d['container-title'] && d['container-title'].length > 0)
-          _reference.journal = d['container-title'][0];
-
-        if (d['published-print'] && d['published-print']['date-parts'] &&
-          d['published-print']['date-parts'][0] && d['published-print']['date-parts'][0][0])
-          _reference.year = d['published-print']['date-parts'][0][0];
-        else if (d['published-online'] && d['published-online']['date-parts'] &&
-          d['published-online']['date-parts'][0] && d['published-online']['date-parts'][0][0])
-          _reference.year = d['published-online']['date-parts'][0][0];
-        else if (d['accepted'] && d['accepted']['date-parts'] &&
-          d['accepted']['date-parts'][0] && d['accepted']['date-parts'][0][0])
-          _reference.year = d['accepted']['date-parts'][0][0];
-
-        if (d.subject)
-          _reference.keywords = d.subject;
-
-        if (d.author && d.author.length === 1)
-          _reference.citation = d.author[0].family;
-        else if (d.author && d.author.length === 2)
-          _reference.citation = d.author[0].family + ' & ' + d.author[1].family;
-        else if (d.author && d.author.length > 2)
-          _reference.citation = d.author[0].family + ' et al.';
-        if (_reference.year)
-          _reference.citation += ' (' + _reference.year + ')';
-
-        if (d.author)
-          _reference.authors = d.author.map((a) => {
-            let author = {};
-            author.family = (a.family == a.family.toUpperCase() ? _.startCase(_.lowerCase(a.family)) : a.family);
-            author._name = (_.trim(a.given) === '' ? '' : a.given.toUpperCase().substr(0, 1) + '. ') + a.family;
-            if (a.given) author.given = a.given;
-            if (a.affiliation && a.affiliation.length > 0)
-              author.affiliation = a.affiliation.map((affiliation) => affiliation.name);
-            if (a.ORCID) {
-              let match = a.ORCID.match(/\/([^\/]+)$/);
-              if (match.length >= 2) author._orcid = match[1];
-            }
-            return author;
-          });
-
-        if (d.author)
-          _reference.long_authors = d.author.map((a) => (a.given ? a.given + ' ' : '') + a.family).join(', ');
-
-        if (d['is-referenced-by-count'])
-          _reference.n_citations = d['is-referenced-by-count'];
-
-        _reference.html = '<b>' +
-          (_reference.long_authors ? _reference.long_authors : '<i>Unknown Authors</i>') +
-          ' (' + (_reference.year ? _reference.year : '<i>Unknown Year</i>') + ').</b> ' +
-          (_reference.title ? _reference.title : '<i>Unknown Title</i>') + '. <i>' +
-          (_reference.journal ? _reference.journal : 'Unknown Journal') +
-          (d.volume ? ' ' + d.volume : '') +
-          (d.issue ? ' (' + d.issue + ')' : '') +
-          (d.page ? ':' + d.page : '') + '.' +
-          (d.DOI ? ' doi:<a href="//dx.doi.org/' + _.toUpper(d.DOI) + '">' + _.toUpper(d.DOI) + '</a>.' : '') +
-          '</i>';
-        _reference.html = _reference.html.replace(/"/g, "'");
-        //console.log("getReferenceMetadata", attempt, `http://api.crossref.org/works/${doi}`, _reference);
-        return _reference;
-
+    async getReferenceMetadata(doi) {
+      try {
+        return await Meteor.call("getCrossrefMetadata", doi);
+      } catch (error) {
+        return await Meteor.call("getDataciteMetadata", doi);
       }
+    },
+
+    async getDataciteMetadata(doi, attempt = 1) {
+      this.unblock();
+      //console.log("getDataciteMetadata", attempt, `http://api.crossref.org/works/${doi}`);
+      let resp;
+      try {
+        resp = HTTP.call("GET", `https://api.datacite.org/dois/${doi}`);
+        if (!resp.statusCode || !resp.data || !resp.data.data || !resp.data.data.attributes) throw "No data";
+      } catch (e) {
+        if (attempt < 3) {
+          return Meteor.call("getDataciteMetadata", doi, attempt + 1);
+        } else {
+          console.error("getDataciteMetadata", `Failed to retrieve reference metadata for doi ${doi}`);
+          throw new Meteor.Error("getDataciteMetadata", `Failed to retrieve reference metadata for doi ${doi}`);
+        }
+      }
+
+      let d = resp.data.data.attributes;
+      let _reference = {
+        source: 'datacite',
+        doi: _.toUpper(d.doi)
+      };
+      
+      if (d.titles && d.titles.length > 0)
+        _reference.title = d.titles[0].title;
+
+      if (d.publisher)
+        _reference.journal = d.publisher;
+
+      if (d.publicationYear)
+        _reference.year = d.publicationYear;
+
+      if (d.subject)
+        _reference.keywords = d.subject;
+
+      if (d.creators && d.creators.length === 1)
+        _reference.citation = d.creators[0].familyName;
+      else if (d.author && d.creators.length === 2)
+        _reference.citation = d.creators[0].familyName + ' & ' + d.creators[1].familyName;
+      else if (d.author && d.creators.length > 2)
+        _reference.citation = d.creators[0].familyName + ' et al.';
+      if (_reference.year)
+        _reference.citation += ' (' + _reference.year + ')';
+
+      if (d.creators)
+        _reference.authors = d.creators.map((a) => {
+          let author = {};
+          author.family = (a.familyName == a.familyName.toUpperCase() ? _.startCase(_.lowerCase(a.familyName)) : a.familyName);
+          author._name = (_.trim(a.givenName) === '' ? '' : a.givenName.toUpperCase().substr(0, 1) + '. ') + a.familyName;
+          if (a.givenName) author.given = a.givenName;
+          if (a.affiliation && a.affiliation.length > 0)
+            author.affiliation = a.affiliation.map((affiliation) => affiliation.name);
+          return author;
+        });
+
+      if (d.creators)
+        _reference.long_authors = d.creators.map((a) => (a.givenName ? a.givenName + ' ' : '') + a.familyName).join(', ');
+
+      if (d.citationCount)
+        _reference.n_citations = d.citationCount;
+
+      _reference.html = '<b>' +
+        (_reference.long_authors ? _reference.long_authors : '<i>Unknown Authors</i>') +
+        ' (' + (_reference.year ? _reference.year : '<i>Unknown Year</i>') + ').</b> ' +
+        (_reference.title ? _reference.title : '<i>Unknown Title</i>') + '. <i>' +
+        (_reference.journal ? _reference.journal : 'Unknown Journal') +
+        (d.volume ? ' ' + d.volume : '') +
+        (d.issue ? ' (' + d.issue + ')' : '') +
+        (d.page ? ':' + d.page : '') + '.' +
+        (d.DOI ? ' doi:<a href="//dx.doi.org/' + _.toUpper(d.DOI) + '">' + _.toUpper(d.DOI) + '</a>.' : '') +
+        '</i>';
+      _reference.html = _reference.html.replace(/"/g, "'");
+      //console.log("getReferenceMetadata", attempt, `http://api.crossref.org/works/${doi}`, _reference);
+      return _reference;
+    },
+
+    async getCrossrefMetadata(doi, attempt = 1) {
+      this.unblock();
+      //console.log("getCrossrefMetadata", attempt, `http://api.crossref.org/works/${doi}`);
+      let resp;
+      try {
+        resp = HTTP.call("GET", `http://api.crossref.org/works/${doi}`);
+        if (!resp.statusCode || !resp.data || !resp.data.message) throw "No data";
+      } catch (e) {
+        if (attempt < 3) {
+          return Meteor.call("getCrossrefMetadata", doi, attempt + 1);
+        } else {
+          console.error("getCrossrefMetadata", `Failed to retrieve reference metadata for doi ${doi}`);
+          throw new Meteor.Error("getCrossrefMetadata", `Failed to retrieve reference metadata for doi ${doi}`);
+        }
+      }
+
+      let d = resp.data.message;
+      let _reference = {
+        source: 'crossref',
+        doi: _.toUpper(d.DOI)
+      };
+
+      if (d.title && d.title.length > 0)
+        _reference.title = d.title[0];
+
+      if (d['container-title'] && d['container-title'].length > 0)
+        _reference.journal = d['container-title'][0];
+
+      if (d['published-print'] && d['published-print']['date-parts'] &&
+        d['published-print']['date-parts'][0] && d['published-print']['date-parts'][0][0])
+        _reference.year = d['published-print']['date-parts'][0][0];
+      else if (d['published-online'] && d['published-online']['date-parts'] &&
+        d['published-online']['date-parts'][0] && d['published-online']['date-parts'][0][0])
+        _reference.year = d['published-online']['date-parts'][0][0];
+      else if (d['accepted'] && d['accepted']['date-parts'] &&
+        d['accepted']['date-parts'][0] && d['accepted']['date-parts'][0][0])
+        _reference.year = d['accepted']['date-parts'][0][0];
+
+      if (d.subject)
+        _reference.keywords = d.subject;
+
+      if (d.author && d.author.length === 1)
+        _reference.citation = d.author[0].family;
+      else if (d.author && d.author.length === 2)
+        _reference.citation = d.author[0].family + ' & ' + d.author[1].family;
+      else if (d.author && d.author.length > 2)
+        _reference.citation = d.author[0].family + ' et al.';
+      if (_reference.year)
+        _reference.citation += ' (' + _reference.year + ')';
+
+      if (d.author)
+        _reference.authors = d.author.map((a) => {
+          let author = {};
+          author.family = (a.family == a.family.toUpperCase() ? _.startCase(_.lowerCase(a.family)) : a.family);
+          author._name = (_.trim(a.given) === '' ? '' : a.given.toUpperCase().substr(0, 1) + '. ') + a.family;
+          if (a.given) author.given = a.given;
+          if (a.affiliation && a.affiliation.length > 0)
+            author.affiliation = a.affiliation.map((affiliation) => affiliation.name);
+          if (a.ORCID) {
+            let match = a.ORCID.match(/\/([^\/]+)$/);
+            if (match.length >= 2) author._orcid = match[1];
+          }
+          return author;
+        });
+
+      if (d.author)
+        _reference.long_authors = d.author.map((a) => (a.given ? a.given + ' ' : '') + a.family).join(', ');
+
+      if (d['is-referenced-by-count'])
+        _reference.n_citations = d['is-referenced-by-count'];
+
+      _reference.html = '<b>' +
+        (_reference.long_authors ? _reference.long_authors : '<i>Unknown Authors</i>') +
+        ' (' + (_reference.year ? _reference.year : '<i>Unknown Year</i>') + ').</b> ' +
+        (_reference.title ? _reference.title : '<i>Unknown Title</i>') + '. <i>' +
+        (_reference.journal ? _reference.journal : 'Unknown Journal') +
+        (d.volume ? ' ' + d.volume : '') +
+        (d.issue ? ' (' + d.issue + ')' : '') +
+        (d.page ? ':' + d.page : '') + '.' +
+        (d.DOI ? ' doi:<a href="//dx.doi.org/' + _.toUpper(d.DOI) + '">' + _.toUpper(d.DOI) + '</a>.' : '') +
+        '</i>';
+      _reference.html = _reference.html.replace(/"/g, "'");
+      //console.log("getReferenceMetadata", attempt, `http://api.crossref.org/works/${doi}`, _reference);
+      return _reference;
     }
   });
 
