b81ba2053f9b75c72d2aa22fd7c931a7b2ab9a71
Rupert Minnett <rminnett@gmail.com>
2026-09-01 07:10:10 +0000

    Implement dynamic loading of Semantic UI stylesheet

--- client/main.js
+++ client/main.js
@@ -4,7 +4,13 @@ import React from 'react';
 import {render} from 'react-dom';
 
 import App from './app';
+import includeSemanticCss from './lib/semantic-ui/include_semantic_css';
+
+// Start fetching Semantic UI right away, then render once it has arrived.
+const semanticCssLoaded = includeSemanticCss();
 
 Meteor.startup(() => {
-  render(<App/>, document.getElementById('react-root'));
+  semanticCssLoaded.then(() => {
+    render(<App/>, document.getElementById('react-root'));
+  });
 });
